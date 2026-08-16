a0204d9d56de92329d9656c0dd9e10b7409996ac
cyberlieflife <cyberlieflife@users.noreply.github.com>
2026-08-17 07:43:44 +0800

    release: v1.4.1

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // Version is the release version (override with -ldflags).
-var Version = "1.4.0"
+var Version = "1.4.1"
 
 // BuildTime can be injected via ldflags.
 var BuildTime = "dev"
